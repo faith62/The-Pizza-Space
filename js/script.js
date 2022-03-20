e8d65a468ba48bf662b30d6dc0a42ad162568040
faith62 <phifeichemu@gmail.com>
2022-03-20 21:41:36 +0300

    add event at order button get  user input then append a new row on the table on the info

--- js/script.js
+++ js/script.js
@@ -2,24 +2,52 @@
 
 
 $(document).ready(function(){
-    $("#order").submit(function(e){
+    $("#order").click(function(e){
         e.preventDefault();
 
         let size =$("#pizza-size").find(":selected").text();
         let crust =$("#pizza-crust").find(":selected").text();
         let tooping =$("#pizza-tooping").find(":selected").text();
+        let order =1;
+        let totalPrice = 0;
 
-        console.log(size);
+    $("").html($(".size option:selected").text()+ "-"+sizeOfPizza); 
+    $("").html($(".tooping option:selected").text()+ "-"+sizeOfPizza);
+    $("").html($(".crust option:selected").text()+ "-"+sizeOfPizza);
+    $("").html(total);  
 
     })
 //    Order constructor
-    function Order(size,crust,tooping){
-        this.size =size
-        this.crust=crust
-        this.tooping=tooping
+    function Pizza(size,crust,tooping,total, orderNo){
+        this.size =size;
+        this.crust=crust;
+        this.tooping=tooping;
+        this.total=total;
+        this.orderNo=orderNo
     }
     
-    Order.prototype.calculatePrice = function()
+    Order.prototype.calculatePrice = function(){}
+
+    $("#add-order").click(function(){
+        
+        let size =$("#pizza-size").find(":selected").text();
+        let crust =$("#pizza-crust").find(":selected").text();
+        let tooping =$("#pizza-tooping").find(":selected").text();
+        let order =1;
+        totalPrice = totalPrice + total;
+
+        let newPizza =new Pizza(size, crust, tooping,total,order);
+
+        let newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' +$("#pizza-size").find(":selected").text() + " - " + newPizza.size + '</td><td id="toppings">' +$("#pizza-tooping").find(":selected").text() + " - " + newPizza.toopings + '</td><td id="crust">' + $("#pizza-crust").find(":selected").text()+ " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+        $("table").append(newRow)
+    });
+
+    // $("#checkout").click(function(){
+    //     totalPrice = totalPrice + total;
+    //     $("").html(totalPrice);
+
+    // });
+
 
 
 })
