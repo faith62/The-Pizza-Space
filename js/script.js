c99d6d08abd6ac54dd82831d9fb3f99740ad9804
faith62 <phifeichemu@gmail.com>
2022-03-21 00:43:14 +0300

    add prototype

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
-
+let totalPizzaPrice;
 
 $(document).ready(function(){
-    $("#order").click(function(e){
-        e.preventDefault();
+    $("#order").click(function(){
+       
 
         let size =$("#pizza-size").find(":selected").text();
         let crust =$("#pizza-crust").find(":selected").text();
@@ -11,24 +11,63 @@ $(document).ready(function(){
         let order =1;
         let totalPrice = 0;
 
-    $("").html($(".size option:selected").text()+ "-"+sizeOfPizza); 
-    $("").html($(".tooping option:selected").text()+ "-"+sizeOfPizza);
-    $("").html($(".crust option:selected").text()+ "-"+sizeOfPizza);
-    $("").html(total);  
+    // $("").html($(".size option:selected").text()+ "-"+sizeOfPizza); 
+    // $("").html($(".tooping option:selected").text()+ "-"+sizeOfPizza);
+    // $("").html($(".crust option:selected").text()+ "-"+sizeOfPizza);
+    // $("").html(total);  
 
-    })
+    });
 //    Order constructor
-    function Pizza(size,crust,tooping,total, orderNo){
+    function Pizza(size,crust,tooping,total,orderNo){
         this.size =size;
         this.crust=crust;
         this.tooping=tooping;
-        this.total=total;
+        this.total= total;
         this.orderNo=orderNo
     }
     
-    Order.prototype.calculatePrice = function(){}
+    Pizza.prototype.totalPrice = function(){
+        let sizePrice;
+        let toopingPrice;
+        let crustPrice;
+        if(this.size=== "small"){
+            sizePrice=600;
+        }
+        else if(this.size === "medium"){
+            sizePrice =800
+        }
+        else{
+            sizePrice=1200
+        }
+
+        if(this.crust=== "crispy"){
+            crustPrice=100;
+        }
+        else if(this.size === "stuffed"){
+            crustPrice=150;
+        }
+        else{
+            crustPrice=200;
+        }
+
+        if(this.tooping === "pepperoni"){
+            toopingPrice=100;
+
+        }
+        else if (this.tooping ==="Mushroom"){
+            toopingPrice=150;
+        }
+        else if (this.tooping ==="Meat Deluxe"){
+            toopingPrice=180;
+        }
+        else{
+            toopingPrice=120;
+        }
+        totalPizzaPrice = sizePrice + crustPrice +toopingPrice;
+    }
 
     $("#add-order").click(function(){
+        
         
         let size =$("#pizza-size").find(":selected").text();
         let crust =$("#pizza-crust").find(":selected").text();
@@ -36,24 +75,36 @@ $(document).ready(function(){
         let order =1;
         totalPrice = totalPrice + total;
 
-        let newPizza =new Pizza(size, crust, tooping,total,order);
+        let newPizza =new Pizza(size, crust, tooping,order);
+        newPizza.totalPrice
 
-        let newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' +$("#pizza-size").find(":selected").text() + " - " + newPizza.size + '</td><td id="toppings">' +$("#pizza-tooping").find(":selected").text() + " - " + newPizza.toopings + '</td><td id="crust">' + $("#pizza-crust").find(":selected").text()+ " - " + newPizza.crust + '</td><td id="total">' + newPizza.total + '</td></tr>'
+        let newRow = '<tr><th scope="row">' + newPizza.orderNo + '</th><td id="size">' +size + " - " + newPizza.size + '</td><td id="crust">' +crust + " - " + newPizza.crust + '</td><td id="tooping">' + tooping+ " - " + newPizza.tooping+ '</td><td id="total">' + newPizza.total + '</td></tr>'
         $("table").append(newRow)
     });
 
     $("#checkout").click(function(){
-        totalPrice = totalPrice + total;
-        $("").html(totalPrice);
+        
+        // totalPrice = totalPrice + total;
+        $(".more-information h4").html(totalPizzaPrice);
 
     });
-    $("button#yes").click(function(){
+    $("#yes").click(function(){
         alert("The delivery charge is : 100")
-        prompt("Please enter your location:")
-        let location= $("prompt").val();
-        alert(`Your Order will be delivered to your ${location}`)
+        
+        let location =prompt("Please enter your location:")
+        alert(`Your Order will be delivered to your location: ${location}`)
+        $(".more-information h4").html(totalPrice + 100);
 
+
+    });
+    $("#no").click(function(){
+        alert("please proceed and click checkout ")
     })
+    $("button#checkout").click(function(){
+        $(".more-infomation-section").hide()
+        $(".more-information h4").html(totalPrice);
+
+    });
 
 
 
